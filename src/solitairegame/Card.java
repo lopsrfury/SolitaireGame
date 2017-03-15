e7c7f05b0a7834fd0b04e75a7730276b6cc1e2c8
John <John@Johns>
2017-03-14 22:57:51 -0600

    adds some code for deck and card

--- src/solitairegame/Card.java
+++ src/solitairegame/Card.java
@@ -10,5 +10,34 @@ package solitairegame;
  * @author John
  */
 public class Card {
+    //Creating card peramiters
+    private String suit;
+    private String rank;
+    private String value;
     
+    //sets up code for what the card contains
+    public card(String cRank, String cSuit, String cValue){
+        rank = cRank;
+        suit = cSuit;
+        value = cValue;
+    }
+    
+    //shows rank
+    public String rank(){
+        return rank;
+    }
+    
+    //shows suit
+    public String suit(){
+        return suit;
+    }
+    
+    //shows value
+    public String value(){
+        return value;
+    }
+    
+    System.out.println suit;
+    System.out.println rank;
+    System.out.println value;
 }
