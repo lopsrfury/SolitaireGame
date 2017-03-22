ead23e69983682271901737742eb495ff24a3a54
lopsrfury <lopsrfury@192.168.1.21>
2017-03-22 21:12:23 +0000

    Fixed getting the same cards

--- src/solitairegame/Dealer.java
+++ src/solitairegame/Dealer.java
@@ -13,12 +13,12 @@ public class Dealer {
         
         //deals from deck to colums
         table.Colum1 = deck.Cards().subList(1,2);
-        table.Colum2 = deck.Cards().subList(1,3);
-        table.Colum3 = deck.Cards().subList(1,4);
-        table.Colum4 = deck.Cards().subList(1,5);
-        table.Colum5 = deck.Cards().subList(1,6);
-        table.Colum6 = deck.Cards().subList(1,7);
-        table.Colum7 = deck.Cards().subList(1,8);
+        table.Colum2 = deck.Cards().subList(2,4);
+        table.Colum3 = deck.Cards().subList(4,7);
+        table.Colum4 = deck.Cards().subList(7,11);
+        table.Colum5 = deck.Cards().subList(11,16);
+        table.Colum6 = deck.Cards().subList(16,22);
+        table.Colum7 = deck.Cards().subList(22,29);
         
         //gives remaing cards to stack
         table.Stack = deck.Cards().subList(1,deck.Cards().size()-1);
