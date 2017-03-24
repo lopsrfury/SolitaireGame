41661f99adc7b3c8df976ac3918a0f976f8bb3ba
lopsrfury <lopsrfury@192.168.1.21>
2017-03-24 18:09:45 +0000

    fixed dealer missing card and updated table viewer to print correctly

--- src/solitairegame/Dealer.java
+++ src/solitairegame/Dealer.java
@@ -12,29 +12,30 @@ public class Dealer {
     public void Init(Table table, Deck deck){
         
        //deals from deck to colums
-       table.Colums.set(0, deck.Cards().subList(1,2));
+       table.Colums.set(0, deck.Cards().subList(0,1));
        table.Colums.get(0).get(table.Colums.get(0).size()-1).flip();
         
-       table.Colums.set(1, deck.Cards().subList(2,4));
+       table.Colums.set(1, deck.Cards().subList(1,3));
        table.Colums.get(1).get(table.Colums.get(1).size()-1).flip();
          
-       table.Colums.set(2, deck.Cards().subList(4,7));
+       table.Colums.set(2, deck.Cards().subList(3,6));
        table.Colums.get(2).get(table.Colums.get(2).size()-1).flip();
         
-       table.Colums.set(3, deck.Cards().subList(7,11));
+       table.Colums.set(3, deck.Cards().subList(6,10));
        table.Colums.get(3).get(table.Colums.get(3).size()-1).flip();
       
-       table.Colums.set(4, deck.Cards().subList(11,16));
+       table.Colums.set(4, deck.Cards().subList(10,15));
        table.Colums.get(4).get(table.Colums.get(4).size()-1).flip();
         
-       table.Colums.set(5, deck.Cards().subList(16,22));
+       table.Colums.set(5, deck.Cards().subList(15,21));
        table.Colums.get(5).get(table.Colums.get(5).size()-1).flip();
         
-       table.Colums.set(6, deck.Cards().subList(22,29));
+       table.Colums.set(6, deck.Cards().subList(21,28));
        table.Colums.get(6).get(table.Colums.get(6).size()-1).flip();
         
+       
        //gives remaing cards to stack
-       table.Stack = deck.Cards().subList(1,deck.Cards().size()-1);
+       table.Stack = deck.Cards().subList(28,deck.Cards().size());
     
     }
 }
--- src/solitairegame/TableViewer.java
+++ src/solitairegame/TableViewer.java
@@ -15,21 +15,44 @@ public class TableViewer {
         System.out.println(table.Colums.get(1));
         System.out.println(table.Colums.get(0));
         */
-        int count = 0 ;
-        for(int i = 0; i < table.Colums.size() * table.Colums.get(6).size(); i++){
-                      
-            
-            //for(int j = 0; j < table.Colums.get(6).size(); j++){
-                if(table.Colums.get(i).size() > count){
-                     System.out.print(table.Colums.get(i).get(count) + " ");
-                }
-                count++;
+       // Print stack, draw and piles
+       System.out.print(table.Stack);
+       System.out.print(table.Draw);
+       System.out.print(table.Cpile);
+       System.out.print(table.Dpile);
+       System.out.print(table.Hpile);
+       System.out.println(table.Spile);
 
-            //}
-             System.out.println("");
+
+         // Print Cards in rows and columns 
+        for(int rowcount = 0; rowcount < table.Colums.get(6).size(); rowcount++){
+        
+            for(int columncount = 0; columncount < table.Colums.size(); columncount++){
+                if(table.Colums.get(columncount).size() > rowcount)
+                {
+                     System.out.print(pad(table.Colums.get(columncount).get(rowcount).toString(),3,' '));
+                }
+                {
+                    System.out.print("   ");
+                }
+
+            }
+            System.out.println("");
             
         }
         
     }
    
+    public String pad(String str, int size, char padChar)
+    {
+      String tmp = str;
+      if( tmp.length() < size)
+      {
+          while( tmp.length() < size  )
+              tmp += padChar;
+      }
+      return tmp;
+    }
+    
+    
 }
